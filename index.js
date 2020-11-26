f7ffc21defdf0a55fc107feda0f22aecaaa1c233
KareemSarhan <Kareemsarhan@gmail.com>
2020-11-26 14:05:11 +0200

    init

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const PORT = process.env.PORT;
-const mongoConnectionString = process.env.mongoConnectionString
-mongoose.connect(mongoConnectionString, { useNewUrlParser: true , useUnifiedTopology: true});
-const connection = mongoose.connection;
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
 
-})
-
-app.use(bodyParser.json());
-
+const app = require('./server.js')
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
