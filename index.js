3dbaec090a2d5e72d8afc2918502225c8abee770
KareemSarhan <Kareemsarhan@gmail.com>
2020-11-26 18:30:49 +0200

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const users = require('./users')
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bycrypt = require('bcryptjs')
 const app = express();
 const mongoConnectionString = process.env.mongoConnectionString
 app.use(bodyParser.json());
@@ -11,19 +12,42 @@ app.use(bodyParser.json());
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
-app.post('/register',(req,res)=>
+app.post('/register',async (req,res)=>
 {
     try {
-        console.log(req.body)
         let {email}=req.body
         if(!email)
         {
             return res.status(400).json({msg:"7ot email 3dl ya wad"})
         }
+        // const salt = await bycrypt.genSalt();
+        // const hashedemail = await bycrypt.hash(email);
+        const newuser = new users(
+        {
+            email:email
+        });
     } catch (error) {
         res.status(500).json({error:error.message})
     }
 
+})
+app.post('/login',async(req,res)=>
+{
+    let {email,password} = req.body;
+    console.log(req.body)
+    console.log(password)
+    if(!email||!password)
+    {
+        return res.status(400).json({msg:"7ot email/password 3dl ya wad"})
+    }    
+    const ExistingUser = await users.findOne({email:email,password:password})
+    if(!ExistingUser){
+        return res.status(400).json({msg:"email mesh mawgood"})
+    }
+    const matching = await bycrypt.compare(password,ExistingUser.password)
+    if(!matching){
+        return res.status(400).json({msg:"el password 8lt"})
+    }
 })
 mongoose.connect(mongoConnectionString, { useNewUrlParser: true , useUnifiedTopology: true});
 const connection = mongoose.connection;
--- users.js
+++ users.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const User = mongoose.Schema
 ({
-email: {type:String,required:true}
+email: {type:String,required:true},
+password: {type:String,required:true}
 })
 module.exports=mongoose.model("user",User)
